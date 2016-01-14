ee77484724c944d35e63aeb45192186284ef5dd0
jonathan sweeney and Marika Allely <jonsweeney26@gmail.com>
2016-01-14 15:15:52 -0800

    add more questions and logic

--- script.js
+++ script.js
@@ -3,10 +3,55 @@ $(function() {
     var morality = $("select#morality").val();
     var cape = $("select#cape").val();
     var friends = $("select#friends").val();
+    var gender = $("select#gender").val();
+    var dog = $("select#dog").val();
+
 
     var hero = null;
     var count = 0;
 
+    if(gender === "MALE") {     //male
+        //morality
+      if (morality === "YES") {
+        //capes
+        if(cape === "YES") {
+          //friends
+          if(friends === "YES"){
+            hero = "Superman";  //cape friends
+          } else {
+            hero = "Batman";    //cape no friends
+          }
+          //no cape
+        } else {
+          if(friends === "YES"){
+            hero = "Iron Man";  //no cape friends
+          } else {
+            hero = "Hulk";      //no cape no friends
+          }
+        }
+
+
+        //morality
+      } else {
+        //capes
+        if(cape === "YES") {
+          //friends
+          if(friends === "YES"){
+            hero = "Magneto";     //cape friends
+          } else {
+            hero = "Dr. Doom";    //cape no friends
+          }
+          //no cape
+        } else {
+          //friends
+          if(friends === "YES"){
+            hero = "Joker";       //no cape friends
+          } else {
+            hero = "Deathstroke"; //no cape no friends
+          }
+        }
+      }
+    } else {                      //female
 
       //morality
     if (morality === "YES") {
@@ -14,39 +59,47 @@ $(function() {
       if(cape === "YES") {
         //friends
         if(friends === "YES"){
-          hero = "Superman";  //cape friends
+          hero = "Power Girl";  //cape friends
         } else {
-          hero = "Batman";    //cape no friends
+          hero = "Batgirl (Cassandra)";    //cape no friends
         }
         //no cape
       } else {
         if(friends === "YES"){
-          hero = "Iron Man";  //no cape friends
+          hero = "Jean Grey";  //no cape friends
         } else {
-          hero = "Hulk";      //no cape no friends
+          hero = "Rogue";      //no cape no friends
         }
       }
 
 
-      //morality
+      //morality (bad)
     } else {
       //capes
       if(cape === "YES") {
         //friends
         if(friends === "YES"){
-          hero = "Magneto";     //cape friends
+          hero = "Emma Frost";     //cape friends
         } else {
-          hero = "Dr. Doom";    //cape no friends
+          hero = "Dark Angel";    //cape no friends
         }
         //no cape
       } else {
         //friends
         if(friends === "YES"){
-          hero = "Joker"; //no cape friends
+          hero = "Harley Quinn"; //no cape friends
         } else {
-          hero = "Deathstroke"; //no cape no friends
+          hero = "Mystique"; //no cape no friends
         }
       }
+    }
+
+
+
+    }
+    //dog
+    if(dog === "YES") {
+      hero = "Krypto";
     }
 
     $("#hero").text(hero);
